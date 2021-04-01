d582536e8b162dd36ace4c0c2ceb6e51b8c4cd4e
Logan Demmy <frank.demmy@gmail.com>
2021-04-01 15:56:53 -0500

    employee departments added

--- src/scripts/employees/employeecard.js
+++ src/scripts/employees/employeecard.js
@@ -1,11 +1,17 @@
 import { getComputer } from "../data/apimanager.js"
 
-export const employeeCard = (element, response) => {
+export const employeeCard = (employee, computer, department) => {
     return `
-    <section class="employeeCard">
-        <h2 class="empName">${element.firstName} ${element.lastName}</h2>
-        <p class="empAge">Age ${element.age}</p>
-        <p class="empComp">${response.model}, manufactured in ${response.year}</p>
+    <div class="employee">
+    <header class="employee__name">
+        <h1>${employee.firstName} ${employee.lastName}</h1>
+    </header>
+    <section class="employee__computer">
+        Currently using a ${computer.year} ${computer.model}
     </section>
+    <section class="employee__department">
+        Works in the ${department.departmentName} department
+    </section>
+</div>
     `
 }
--- src/scripts/employees/employeelist.js
+++ src/scripts/employees/employeelist.js
@@ -1,10 +1,11 @@
 import { employeeCard } from "./employeecard.js"
 
-export const getEmployeeList = (allEmployees, compute) => {
+export const getEmployeeList = (allEmployees, allComputer, allDepartment) => {
     let employeeHTML = ""
     allEmployees.forEach(employ => {
-        let emplyComp = compute.find(computer => computer.id === employ.computerId)
-        employeeHTML += employeeCard(employ, emplyComp);
+        let emplyComp = allComputer.find(computer => computer.id === employ.computerId)
+        let emplyDept = allDepartment.find(department => department.id === employ.departmentId)
+        employeeHTML += employeeCard(employ, emplyComp, emplyDept);
     })
     return employeeHTML;
 }
